06d0b20d51844f00cd935a68d6e63549d9760e5b
Kovács Gergő <gergo.kovacs@bookrkids.hu>
2021-04-30 14:27:49 +0200

    Mobile optimalization

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,14 +7,14 @@ var Home = () =>
 <div className={styles.page}>
     <div>
         <div>
-            <img src={image} alt="" className={styles.person} />
-        </div>
-        <div>
             <h2>Kovács Gergő</h2>
             <div>Software Engineer</div>
             <div>kiskovi97(at)gmail.com</div>
             <div>https://github.com/kiskovi97</div>
             <div>https://www.instagram.com/kiskovi9706/</div>
+        </div>
+        <div>
+            <img src={image} alt="" className={styles.person} />
         </div>
     </div>
     <div>
