6bf90b47384cd7976931f750373dc7a3cc909d52
Kovács Gergő <gergo.kovacs@bookrkids.hu>
2021-05-06 20:43:19 +0200

    Fact check

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import styles from './Page.module.css'
 import Me from './Components/Me'
 import gStyles from './Grid.module.css'
 import ScrollAnimation from 'react-animate-on-scroll'
-import {Bookr} from './Icons'
+import {Bookr, BME, Group} from './Icons'
 
 var About = () => 
 <div className={styles.page}> 
@@ -11,14 +11,49 @@ var About = () =>
     <div className={gStyles.grid}>
         <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutLeft" animateOnce >
             <h3>Currently working at:</h3>
-            <div>2020 - Bookr Kids - Unity developer</div>
+            <h4>2020 - Bookr Kids - Unity developer</h4>
             <h3>Previous Jobs</h3>
             <div>2019 - 2020 Evosoft - .net developer</div>
-            <div>2018 - 2019 Atoldev - java developer</div>
+            <div>2018 Atoldev - Fullstack developer in java</div>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInRight" animateOut="fadeOutRight" animateOnce >
             <div className={styles.logo}>
                 <Bookr />
+            </div>
+        </ScrollAnimation>
+    </div>
+    <div className={gStyles.grid}>
+        <ScrollAnimation animateIn="fadeInRight" animateOut="fadeOutRight" animateOnce >
+            <div className={styles.logo}>
+                <BME />
+            </div>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutLeft" animateOnce >
+            <h3>2015-2019</h3>
+            <div>Budapest University of Technology and Economics</div>
+            <div>Computer Engineering BSc</div>
+            <h3>2019 - 2021</h3>
+            <div>Budapest University of Technology and Economics</div>
+            <div>Computer Engineering MSc</div>
+            <h3>2016 - 2021</h3>
+            <div>Simonyi Károly College for Advanced Studies</div>
+            <div>Schönherz Design Studio</div>
+        </ScrollAnimation>
+    </div>
+    <div className={gStyles.grid}>
+        <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutLeft" animateOnce >
+            <h2>University Self-active groups</h2>
+            <h3>ParkettKlub</h3>
+            <li>Public Relations</li>
+            <li>Event Orginizer</li>
+            <li>Leader</li>
+            <h3>Schönherz Design Studio</h3>
+            <li>3D Modelling Mentor</li>
+            <li>Graphic and UI Designer</li>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInRight" animateOut="fadeOutRight" animateOnce >
+            <div className={styles.logo}>
+                <Group />
             </div>
         </ScrollAnimation>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import styles from './Page.module.css'
 import gStyles from './Grid.module.css'
 import Me from './Components/Me'
+import {Skill} from './Icons'
 
 var Home = () => 
 <div className={styles.page}>
@@ -10,9 +11,22 @@ var Home = () =>
         <Me/>
     </div>
     <div className={gStyles.grid}>
-        <ScrollAnimation  animateIn="fadeInLeft" animateOut="fadeOutLeft" animateOnce>
-            <h3>Hey visiter</h3>
-            <div>I am a softwere Engineer. I work mostly with unity, but as you see I know some basic React too</div>
+        <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutLeft" animateOnce >
+            <h2>Skills</h2>
+            <h3>Programming Languages</h3>
+            <div>C, C++, C#, Java, Html, Css, Javascript, SQL</div>
+            <h3>Software Skills</h3>
+            <div>Blender3D, 3DSMax, AndroidStudio,
+            AdobeIllustrator, AdobePhotoshop,
+            Unity, Oracle, React, Angular,WebGL
+            MicrosoftSQLServer, .NET</div>
+            <h3>OtherSkills</h3>
+            <div>Public Relations, ProjectManagment, GraphicDesign</div>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInRight" animateOut="fadeOutRight" animateOnce >
+            <div className={styles.logo}>
+                <Skill />
+            </div>
         </ScrollAnimation>
     </div>
 </div>
