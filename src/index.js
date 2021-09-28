a38009fbca63a2ef3a750fc0401d9f36b011a10b
Kovács Gergő <gergo.kovacs@bookrkids.hu>
2021-09-28 14:18:40 +0200

    Added stars

--- src/index.js
+++ src/index.js
@@ -4,6 +4,63 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import * as Three from 'three';
+
+const scene = new Three.Scene();
+const camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 50, 1000);
+const renderer = new Three.WebGL1Renderer({
+  alpha: true,
+  canvas: document.querySelector('#bg'),
+});
+
+const fCamera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 10, 50);
+const fRenderer = new Three.WebGL1Renderer({
+  alpha: true,
+  canvas: document.querySelector('#fg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+fRenderer.setPixelRatio(window.devicePixelRatio);
+fRenderer.setSize(window.innerWidth, window.innerHeight);
+
+function addStar() {
+  const geometry = new Three.SphereGeometry(0.25,24,24);
+  const material = new Three.MeshStandardMaterial({color: 0xFFFFFF});
+  const star = new Three.Mesh(geometry, material);
+
+  const [x,y,z] = Array(3).fill().map(() => Three.MathUtils.randFloatSpread(300));
+  star.position.set(x,y * 2,z);
+  scene.add(star);
+}
+
+Array(1000).fill().forEach(addStar);
+
+const ambientLight = new Three.AmbientLight(0xFFFFFF);
+scene.add(ambientLight);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+
+  renderer.render(scene, camera);
+  fRenderer.render(scene, fCamera);
+}
+
+animate();
+
+function moveCamera() {
+    const t = document.body.getBoundingClientRect().top;
+
+
+    camera.rotation.x = t * 0.0001;
+    fCamera.rotation.x = t * 0.0001;
+
+    camera.position.y = t * 0.03;
+    fCamera.position.y = t * 0.03;
+}
+
+document.body.onscroll = moveCamera;
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -35,6 +35,15 @@ var About = () =>
                     <a href="https://thebookclub.page.link/app?Library" className={styles.minilogo}><h3>Library</h3></a>
                     <a href="https://thebookclub.page.link/app?BookDetails:41" className={styles.minilogo}><h3>BookDeatails : Adelita</h3></a>
                     <a href="https://thebookclub.page.link/app?BookDetails:42" className={styles.minilogo}><h3>BookDeatails : Race around</h3></a>
+            <h1>The Book Club Application link test (thebookclub:\\)</h1>
+                    <a href="thebookclub://app" className={styles.minilogo}><h3>Application</h3></a>
+                    <a href="thebookclub://app?Dashboard" className={styles.minilogo}><h3>Dashboard</h3></a>
+                    <a href="thebookclub://app?Library" className={styles.minilogo}><h3>Library</h3></a>
+                    <a href="thebookclub://app?BookDetails:41" className={styles.minilogo}><h3>BookDeatails : Adelita</h3></a>
+                    <a href="thebookclub://app?BookDetails:42" className={styles.minilogo}><h3>BookDeatails : Race around</h3></a>
+            <form name="myform" action="https://envch2uqthjfl5b.m.pipedream.net/" method="post">
+                <button>Post Deeplink test</button>
+            </form>
         </ScrollAnimation>
     </div>
     <div className={gStyles.grid}>
