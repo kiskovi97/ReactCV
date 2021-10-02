eb1094abdd9b4f7542fd01e328c0053214677ccd
Kovács Gergő <gergo.kovacs@bookrkids.hu>
2021-10-02 16:21:49 +0200

    About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ import Me from './Components/Me'
 import gStyles from './Grid.module.css'
 import ScrollAnimation from 'react-animate-on-scroll'
 import { Bookr, BME, Group } from './Icons'
+import fb from '../logos/facebook.svg'
 
 var About = () =>
     <div className={styles.page}>
@@ -12,14 +13,55 @@ var About = () =>
             <div className={gStyles.grid}>
                 <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutLeft" animateOnce >
                     <h3>Currently working at:</h3>
-                    <h4>2020 - Bookr Kids - Unity developer</h4>
+                    <h4>2020 - <a href={"https://corp.bookrkids.com/"}>Bookr Kids</a> - Unity developer</h4>
                     <h3>Previous Jobs</h3>
-                    <div>2019 - 2020 Evosoft - .net developer</div>
-                    <div>2018 Atoldev - Fullstack developer in java</div>
+                    <h4>2019 - 2020 <a href={"https://www.evosoft.hu/"}>Evosoft</a> - .NET developer</h4>
+                    <h4>2018 - 2019 <a href={"https://atolldev.com/}"}>Atoldev</a> - Java fullstack developer</h4>
                 </ScrollAnimation>
                 <ScrollAnimation animateIn="fadeInRight" animateOut="fadeOutRight" animateOnce >
                     <div className={styles.logo}>
                         <Bookr />
+                    </div>
+                </ScrollAnimation>
+            </div>
+        </div>
+        
+        <div>
+            <div className={gStyles.grid}>
+                <ScrollAnimation animateIn="fadeInRight" animateOut="fadeOutRight" animateOnce >
+                    <div className={styles.logo}>
+                        <BME />
+                    </div>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutLeft" animateOnce >
+                    <h3>2015 - 2019</h3>
+                    <h4>Computer Engineering BSc</h4>
+                    <div>Budapest University of Technology and Economics</div>
+                    <h3>2019 - 2021</h3>
+                    <h4>Computer Engineering MSc</h4>
+                    <div>Budapest University of Technology and Economics</div>
+                    <h3>2016 - 2021</h3>
+                    <h4>Schönherz Design Studio</h4>
+                    <div>Simonyi Károly College for Advanced Studies</div>
+                </ScrollAnimation>
+            </div>
+        </div>
+        <div>
+            <div className={gStyles.grid}>
+                <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutLeft" animateOnce >
+                    <h2>University Self-active groups</h2>
+                    <h3><a href={"https://www.facebook.com/ParkettKlub/"}>ParkettKlub</a></h3>
+                    <li>Public Relations</li>
+                    <li>Event Orginizer</li>
+                    <li>Leader</li>
+                    
+                    <h3><a href={"https://schdesign.hu/"}>Schönherz Design Studio</a></h3>
+                    <li>3D Modelling Mentor</li>
+                    <li>Graphic and UI Designer</li>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="fadeInRight" animateOut="fadeOutRight" animateOnce >
+                    <div className={styles.logo}>
+                        <Group />
                     </div>
                 </ScrollAnimation>
             </div>
@@ -50,46 +92,6 @@ var About = () =>
                 </ScrollAnimation>
             </div>
         </div>
-        <div>
-            <div className={gStyles.grid}>
-                <ScrollAnimation animateIn="fadeInRight" animateOut="fadeOutRight" animateOnce >
-                    <div className={styles.logo}>
-                        <BME />
-                    </div>
-                </ScrollAnimation>
-                <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutLeft" animateOnce >
-                    <h3>2015-2019</h3>
-                    <div>Budapest University of Technology and Economics</div>
-                    <div>Computer Engineering BSc</div>
-                    <h3>2019 - 2021</h3>
-                    <div>Budapest University of Technology and Economics</div>
-                    <div>Computer Engineering MSc</div>
-                    <h3>2016 - 2021</h3>
-                    <div>Simonyi Károly College for Advanced Studies</div>
-                    <div>Schönherz Design Studio</div>
-                </ScrollAnimation>
-            </div>
-        </div>
-        <div>
-            <div className={gStyles.grid}>
-                <ScrollAnimation animateIn="fadeInLeft" animateOut="fadeOutLeft" animateOnce >
-                    <h2>University Self-active groups</h2>
-                    <h3>ParkettKlub</h3>
-                    <li>Public Relations</li>
-                    <li>Event Orginizer</li>
-                    <li>Leader</li>
-                    <h3>Schönherz Design Studio</h3>
-                    <li>3D Modelling Mentor</li>
-                    <li>Graphic and UI Designer</li>
-                </ScrollAnimation>
-                <ScrollAnimation animateIn="fadeInRight" animateOut="fadeOutRight" animateOnce >
-                    <div className={styles.logo}>
-                        <Group />
-                    </div>
-                </ScrollAnimation>
-            </div>
-        </div>
-
     </div>
 
 export default About;
